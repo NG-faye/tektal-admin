dac5a8cb1a1f259c93e72323d980d192f1ee8245
Nogaye Ndao <nogayendao2020@gmail.com>
2026-02-23 15:10:58 +0000

    hardcode production URL

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://tektal-backend.onrender.com";
 
 const api = axios.create({
   baseURL: `${BASE_URL}/admin-panel/api/`,
