3da61cf5aace7e334ece93d098ec7d9a6c3cb6dc
Nogaye Ndao <nogayendao2020@gmail.com>
2026-02-23 13:37:44 +0000

    add vercel.json

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tektal-backend.onrender.com";
+  baseURL: "https://tektal-backend.onrender.com",
 });
 
 // ✅ Refresh automatique du token
